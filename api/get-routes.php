92bf5a14773c57bb765a985ea7665a6a2794b4c1
Wiktor Maj <wiktor.maj@schibsted.pl>
2017-02-09 21:57:29 +0100

    Dodanie pola z flagą aktualnego roku do bazy danych API.

--- api/get-routes.php
+++ api/get-routes.php
@@ -10,23 +10,25 @@ try {
 	$result = array();
 	
 	if (!empty($area)) {
-		$stmt = $pdo->prepare('SELECT `id`, `name`, `areaId`, `territoryId` FROM `'.AgentTables::EDK_ROUTES.'` WHERE `areaId` = :areaId ORDER BY `name`');
+		$stmt = $pdo->prepare('SELECT `id`, `name`, `areaId`, `territoryId`, `currentYear` FROM `'.AgentTables::EDK_ROUTES.'` WHERE `areaId` = :areaId ORDER BY `name`');
 		$stmt->bindValue(':areaId', $area);
 		$stmt->execute();
 		while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $row['currentYear'] = $row['currentYear'] || false;
 			$result[] = $row;
 		}
 		$stmt->closeCursor();
 	} elseif(!empty($territory)) {
-		$stmt = $pdo->prepare('SELECT `id`, `name`, `areaId`, `territoryId` FROM `'.AgentTables::EDK_ROUTES.'` WHERE `territoryId` = :territoryId ORDER BY `name`');
+		$stmt = $pdo->prepare('SELECT `id`, `name`, `areaId`, `territoryId`, `currentYear` FROM `'.AgentTables::EDK_ROUTES.'` WHERE `territoryId` = :territoryId ORDER BY `name`');
 		$stmt->bindValue(':territoryId', $territory);
 		$stmt->execute();
 		while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $row['currentYear'] = $row['currentYear'] || false;
 			$result[] = $row;
 		}
 		$stmt->closeCursor();
 	} elseif(!empty($route) && empty($details)) {
-		$stmt = $pdo->prepare('SELECT `id`, `name`, `areaId`, `territoryId`, `publicAccessSlug` AS `kmlData` FROM `'.AgentTables::EDK_ROUTES.'` WHERE `id` = :id');
+		$stmt = $pdo->prepare('SELECT `id`, `name`, `areaId`, `territoryId`, `currentYear`, `publicAccessSlug` AS `kmlData` FROM `'.AgentTables::EDK_ROUTES.'` WHERE `id` = :id');
 		$stmt->bindValue(':id', $route);
 		$stmt->execute();
 		
@@ -36,12 +38,13 @@ try {
                         $fullUrl = 'http://m2.edk.org.pl/mirror/'.$row['kmlData'].'/edk-gps-route-'.$row['id'].'.kml';
 			
 			$result = $row;
+            $result['currentYear'] = $row['currentYear'] || false;
 			$result['kmlDataPath'] = $fullUrl;
 			$result['stations'] = array();
 		}
 		$stmt->closeCursor();
 	} elseif(!empty($route) && $details == 'full') {
-		$stmt = $pdo->prepare('SELECT r.`id`, r.`name`, r.`areaId`, r.`territoryId`, r.`publicAccessSlug` AS `kmlData`, n.content AS `content` '
+		$stmt = $pdo->prepare('SELECT r.`id`, r.`name`, r.`areaId`, r.`territoryId`, r.`currentYear`, r.`publicAccessSlug` AS `kmlData`, n.content AS `content` '
 			. 'FROM `'.AgentTables::EDK_ROUTES.'` r '
 			. 'LEFT JOIN `'.AgentTables::EDK_ROUTE_NOTES.'` n ON (n.routeId = r.id AND n.noteType = 1) WHERE r.`id` = :id');
 		$stmt->bindValue(':id', $route);
@@ -59,6 +62,7 @@ try {
 					'areaId' => $row['areaId'],
 					'name' => $row['name'],
 					'kmlDataPath' => $fullUrl,
+                    'currentYear' => $row['currentYear'] || false,
 				),
 				'stationDescs' => array(),
 			);
