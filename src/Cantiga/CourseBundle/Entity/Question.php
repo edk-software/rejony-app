7091f847d592732642fd3a26a37cd3cf272b00fd
zaquncio <gazorek1@interia.pl>
2017-02-10 13:10:11 +0100

    Bugfix: generowanie formularza testu

--- src/Cantiga/CourseBundle/Entity/Question.php
+++ src/Cantiga/CourseBundle/Entity/Question.php
@@ -54,7 +54,7 @@ class Question
 			throw new ModelException('Cannot create a question with no correct answers!');
 		}
 	}
-	
+
 	/**
 	 * Generates a single form field for the test form.
 	 * 
@@ -63,7 +63,7 @@ class Question
 	 */
 	public function generateFormField(FormBuilderInterface $fbi, $idx)
 	{
-		$fbi->add($this->generateFieldName($idx), new ChoiceType, array(
+		$fbi->add($this->generateFieldName($idx), ChoiceType::class, array(
 			'label' => $this->content,
 			'expanded' => true,
 			'multiple' => $this->correctAnswerNum != 1,
@@ -166,8 +166,8 @@ class Question
 	private function answersToArray()
 	{
 		$result = array();
-		foreach($this->answers as $answer) {
-			$result[] = $answer->getAnswer();
+		foreach($this->answers as $key=>$answer) {
+			$result[$answer->getAnswer()] = $key;
 		}
 		return $result;
 	}
