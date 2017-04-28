209eb2113118a12701ed7e0b77b231c0947c66b9
Wiktor Maj <wiktor.maj@schibsted.pl>
2017-04-28 23:24:50 +0200

    Remove API (moved to separate repository).

--- api/config.dist.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_HOST', '');
-define('DB_NAME', '');
-define('DB_PASS', '');
-define('DB_USER', '');
-
-define('ENCRYPTION_ALGORITHM', 'aes-256-cbc');
-define('ENCRYPTION_KEY', '');
