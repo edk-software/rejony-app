6ad055d6fa9989902696e593dd5d27f279ce2ed5
zaquncio <gazorek1@interia.pl>
2017-10-28 14:43:00 +0200

    Stylowanie mapy w zgłoszeniach

--- web/js/map-marker.js
+++ web/js/map-marker.js
@@ -2,7 +2,68 @@ function render(data) {
     var myLatLng = {lat: 50.043, lng: 19.498};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 5,
-        center: myLatLng
+        center: myLatLng,
+        styles: [{
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [{"color": "#444444"}]
+        }, {
+            "featureType": "administrative.land_parcel",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}]
+        }, {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [{"color": "#f2f2f2"}]
+        }, {
+            "featureType": "landscape.natural",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}]
+        }, {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [{"visibility": "on"}, {"color": "#052366"}, {"saturation": "-70"}, {"lightness": "85"}]
+        }, {
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [{"visibility": "simplified"}, {"lightness": "-53"}, {"weight": "1.00"}, {"gamma": "0.98"}]
+        }, {
+            "featureType": "poi",
+            "elementType": "labels.icon",
+            "stylers": [{"visibility": "simplified"}]
+        }, {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [{"saturation": -100}, {"lightness": 45}, {"visibility": "on"}]
+        }, {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{"saturation": "-18"}]
+        }, {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [{"visibility": "off"}]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers": [{"visibility": "on"}]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "all",
+            "stylers": [{"visibility": "on"}]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "labels.icon",
+            "stylers": [{"visibility": "off"}]
+        }, {
+            "featureType": "road.local",
+            "elementType": "all",
+            "stylers": [{"visibility": "on"}]
+        }, {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}]
+        }, {"featureType": "water", "elementType": "all", "stylers": [{"color": "#57677a"}, {"visibility": "on"}]}]
     });
     for (i = 0; i < data.length; i++) {
         var place = data[i];
@@ -13,7 +74,7 @@ function render(data) {
             position: {lat: Number(place.lat), lng: Number(place.lng)},
             map: map,
             title: place.name,
-            draggable: false,
+            draggable: false
         });
 
 
