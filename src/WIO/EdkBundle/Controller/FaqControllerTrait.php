09ea8a887bfdd75fce39928947d62d0c48b6c209
Wiktor Maj <wiktor.maj@schibsted.pl>
2017-11-24 03:51:36 +0100

    Change breadcrumb name.

--- src/WIO/EdkBundle/Controller/FaqControllerTrait.php
+++ src/WIO/EdkBundle/Controller/FaqControllerTrait.php
@@ -28,7 +28,7 @@ trait FaqControllerTrait
         $this
             ->breadcrumbs()
             ->workgroup($workgroup)
-            ->entryLink($this->trans('Help', [], 'pages'), $this->indexRouteName, [
+            ->entryLink($this->trans('Frequently asking questions', [], 'pages'), $this->indexRouteName, [
                 'slug' => $this->getSlug(),
             ])
         ;
