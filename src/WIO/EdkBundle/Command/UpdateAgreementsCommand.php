954c64849ecc7a9620a78583b93949645414a101
zaquncio <gazorek1@interia.pl>
2020-03-08 15:46:39 +0100

    Wyłączenie automatyczych downgradów umów

--- src/WIO/EdkBundle/Command/UpdateAgreementsCommand.php
+++ src/WIO/EdkBundle/Command/UpdateAgreementsCommand.php
@@ -68,7 +68,7 @@ class UpdateAgreementsCommand extends ContainerAwareCommand
                 $result[self::toUpdated][] = $row;
             }
             if ($row->isContractToBeDowngrade()) {
-                $repository->updateContract($row->getAreaId(), 0);
+//                $repository->updateContract($row->getAreaId(), 0);
                 $result[self::toDowngrade][] = $row;
             }
         }
